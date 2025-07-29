ef36fc73db970500a2f8a7d27a75383e45ffb4b3
ryukyoung <doggy1103@naver.com>
2025-07-29 16:33:55 +0900

    날씨 요약 + 날씨 정보 박스 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,74 @@
-import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
-import { Dimensions, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const getWeatherInfo = async (latitude: number, longitude: number) => {
+  const apiKey = process.env.EXPO_PUBLIC_WEATHER_API_KEY;
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=metric&lang=kr&exclude=alerts&appid=${apiKey}`
+    );
+    const data = await response.json();
+    //console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error("날씨 정보 API 호출 실패: " + error);
+    return null;
+  }
+};
+
+const convertDate = (timestamp: number) => {
+  const date = new Date(timestamp * 1000);
+  // 한국 시간대로 포맷팅
+  const formattedDate = date.toLocaleDateString("ko-KR", {
+    timeZone: "Asia/Seoul",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return formattedDate;
+};
 
 const getGoogleMapGeocode = async (latitude: number, longitude: number) => {
   const apiKey = process.env.EXPO_PUBLIC_API_KEY;
   try {
     const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
     );
-  const data = await response.json();
-  console.log(data);
-  
-  return data;
-} catch (error) {
-  console.error("구글 맵 API 호출 실패: " + error);
-  return null;
-}
-}
+    const data = await response.json();
+    console.log(data);
 
-const {width: SCREEN_WIDTH} = Dimensions.get("window");
+    return data;
+  } catch (error) {
+    console.error("구글 맵 API 호출 실패: " + error);
+    return null;
+  }
+};
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function Index() {
-  const [ok, setOk ] = useState<boolean>(false);
-  const[errorMsg, setErrorMsg] = useState<string>("");
-  const[city, setCity] = useState<string | null>(""); 
-  
-  const locationData = async () => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
+  const [ok, setOk] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [city, setCity] = useState<string | null>("");
+  const [DailyWeatherData, setDailyWeatherData] = useState<Array<any>>([]);
 
-    if(!granted) {
+  const locationData = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
       setOk(false);
       setErrorMsg("위치에 대한 권한 부여가 거부되었습니다.");
       return;
@@ -36,19 +76,22 @@ export default function Index() {
 
     const location = await Location.getCurrentPositionAsync({});
     const { latitude, longitude } = location.coords;
-    console.log(latitude, longitude);
+    // console.log(latitude, longitude);
 
     const address = await getGoogleMapGeocode(latitude, longitude);
-    console.log(address);
-    console.log(address.results[4].formatted_address);
+    //console.log(address);
+    // console.log(address.results[4].formatted_address);
 
     const cityAddress = address.results[4].formatted_address;
     const citySplit = cityAddress.split(" ");
-    console.log(citySplit);
+    // console.log(citySplit);
 
-    const city = `${citySplit[1]} ${citySplit[2]}`
+    const city = `${citySplit[1]} ${citySplit[2]}`;
     setCity(city);
 
+    const weatherData = await getWeatherInfo(latitude, longitude);
+
+    setDailyWeatherData(weatherData.daily);
     return;
   };
 
@@ -58,80 +101,99 @@ export default function Index() {
 
   return (
     <>
-    <StatusBar backgroundColor="skyblue" barStyle="dark-content" />
-    <View style={[styles.container]}>
-      <View style={styles.cityWrap}>
-        <Text style={styles.cityName}>{city}</Text>
+      <StatusBar backgroundColor="skyblue" barStyle="dark-content" />
+      <View style={[styles.container]}>
+        <View style={styles.cityWrap}>
+          <Text style={styles.cityName}>{city}</Text>
+        </View>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          contentContainerStyle={styles.mainContentView}
+        >
+          {DailyWeatherData.map((item, index) => (
+            <View style={styles.mainWrap} key={index}>
+              <View style={styles.header}>
+                <Text style={styles.regDate}>{convertDate(item.dt)}</Text>
+
+                <View style={styles.weatherWrap}>
+                  <Text style={styles.weather}>{item.weather[0].main}</Text>
+                  <Image
+                    source={{
+                      uri: `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`,
+                    }}
+                    style={styles.weatherIcon}
+                  />
+                </View>
+              </View>
+              <View style={styles.body}>
+                <View style={styles.tempWrap}>
+                  <Text style={styles.temp}>{item.temp.day.toFixed(0)}</Text>
+                  <Text style={styles.tempUnit}> ° </Text>
+                </View>
+                <View style={styles.summaryWrap}>
+                  <Text style={styles.dailySummaryTitle}>Daily Summary</Text>
+                  <Text style={styles.summaryText}>{item.summary}</Text>
+                </View>
+                <View style={styles.subInfoWrap}>
+                  <View style={styles.subInfoInner}>
+                    <View style={styles.subInfoItem}>
+                      <Feather name="wind" size={24} color="white" />
+                      <Text style={styles.subInfoDes}>
+                        {item.wind_speed.toFixed(1)}km/h
+                      </Text>
+                      <Text style={styles.subInfoWeather}>wind</Text>
+                    </View>
+                    <View style={styles.subInfoItem}>
+                      <Feather name="droplet" size={24} color="white" />
+                      <Text style={styles.subInfoDes}>{item.humidity}%</Text>
+                      <Text style={styles.subInfoWeather}>humidity</Text>
+                    </View>
+                    <View style={styles.subInfoItem}>
+                      <Feather name="eye" size={24} color="white" />
+                      <Text style={styles.subInfoDes}>{item.pop}%</Text>
+                      <Text style={styles.subInfoWeather}>pop</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.footer}></View>
+            </View>
+          ))}
+        </ScrollView>
       </View>
-      <ScrollView horizontal pagingEnabled contentContainerStyle={styles.mainContentView}>
-        <View style={styles.mainWrap}>
-        <View style={styles.header}> 
-            <Text style={styles.regDate}>Fri, 2025-07-25</Text>
-            <Text style={styles.weather}>맑음</Text>
-        </View>
-        <View style={styles.body}> 
-          <Text style={styles.temp}> 35 </Text>
-          <Text style={styles.tempUnit}> ° </Text>
-        </View>
-        <View style={styles.footer}> </View>
-      </View>
-      <View style={styles.mainWrap}>
-        <View style={styles.header}> 
-            <Text style={styles.regDate}>Sat, 2025-07-26</Text>
-            <Text style={styles.weather}>맑음</Text>
-        </View>
-        <View style={styles.body}> 
-          <Text style={styles.temp}>
-            35
-          </Text>
-          <Text style={styles.tempUnit}>
-            °
-          </Text>
-        </View>
-        <View style={styles.footer}> </View>
-      </View>
-      <View style={styles.mainWrap}>
-        <View style={styles.header}> 
-            <Text style={styles.regDate}>Sun, 2025-07-27</Text>
-            <Text style={styles.weather}>맑음</Text>
-        </View>
-        <View style={styles.body}> 
-          <Text style={styles.temp}>
-            35
-          </Text>
-          <Text style={styles.tempUnit}>
-            °
-          </Text>
-        </View>
-        <View style={styles.footer}> </View>
-      </View>
-      </ScrollView>
-      
-    </View>
-     </>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: "skyblue",
   },
+  mainContentView: {},
+  mainWrap: {
+    backgroundColor: "skyblue",
+    borderWidth: 3,
+    borderColor: "red",
+    width: SCREEN_WIDTH,
+    position: "relative",
+  },
   cityWrap: {
-    flex: 0.15,
+    flex: 0.1,
     alignItems: "center",
     justifyContent: "center",
     borderWidth: 3,
   },
-    cityName: {
-      fontSize: 30,
-      fontWeight: "bold",
+  cityName: {
+    fontSize: 28,
+    fontWeight: "bold",
   },
   header: {
-    flex: 0.5,
+    flex: 0.6,
     alignItems: "center",
     justifyContent: "center",
+    position: "relative",
   },
   regDate: {
     fontSize: 15,
@@ -141,46 +203,102 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     padding: 15,
     borderRadius: 50,
+    position: "absolute",
     paddingTop: 5,
     paddingBottom: 5,
+    top: 12,
   },
   weather: {
     fontSize: 23,
     fontWeight: "bold",
-    marginTop: 10,
   },
-  body: {
-    flex: 4,
+  weatherWrap: {
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    position: "absolute",
+    right: 100,
+    top: 34,
+  },
+  weatherIcon: {
+    width: 80,
+    height: 80,
+    marginLeft: -5,
+  },
+  body: {
+    flex: 2.3,
+    borderColor: "green",
     borderWidth: 3,
   },
-  mainContentView: {
-
-  },
-  mainWrap: {
-    backgroundColor: "skyblue",
+  tempWrap: {
+    flex: 0.4,
     borderWidth: 3,
     borderColor: "red",
-    width: SCREEN_WIDTH,
-    position: "relative"
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
   },
   temp: {
     fontSize: 150,
     fontWeight: "bold",
-    right: 20,
-    top: -50,
+    position: "absolute",
+    top: -20,
+    left: 68,
   },
   tempUnit: {
     fontSize: 130,
     position: "absolute",
-    top: 95,
-    right: 45,
+    top: -23,
+    right: 10,
   },
-  footer: {
-    flex: 1,
+  summaryWrap: {
+    flex: 0.27,
     backgroundColor: "skyblue",
     borderWidth: 3,
     borderColor: "red",
-  }
-})
+  },
+  dailySummaryTitle: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 8,
+    marginLeft: 13,
+  },
+  summaryText: {
+    fontSize: 14.5,
+    marginTop: 5,
+    marginLeft: 13,
+    paddingRight: 15,
+  },
+  subInfoWrap: {
+    flex: 0.33,
+    backgroundColor: "skyblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  subInfoInner: {
+    backgroundColor: "black",
+    borderRadius: 17,
+    width: 320,
+    height: 110,
+    flexDirection: "row",
+  },
+  subInfoItem: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  subInfoDes: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginTop: 3,
+    color: "white",
+  },
+  subInfoWeather: {
+    fontSize: 13,
+    color: "white",
+    paddingTop: 0,
+  },
+  footer: {
+    flex: 0.8,
+  },
+});
